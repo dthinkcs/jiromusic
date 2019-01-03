6d5880bea7e7ea7eee41916c4150159be170a106
[dthinkcs] <[deliberatethink@gmail.com]>
2019-01-03 19:06:25 +0530

    Welcome to JIRO Music

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div style="font-family: Hero, sans-serif; text-align: center">
+		<h1>
+			Welcome to JIRO Music
+		</h1>
+	</div>
+</body>
+</html>
